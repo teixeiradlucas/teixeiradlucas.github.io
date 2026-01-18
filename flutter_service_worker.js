67630cc4000cd96160fccc5109b0b0c52fdb11b2
Lucas Francisco Teixeira <63049317+teixeiradlucas@users.noreply.github.com>
2026-01-17 22:28:51 -0300

    atualização

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "85e17cf3715bb6e663dde23167e7709d
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "ca0ab30bded9e88cf2a9e99a49a9fa15",
+"flutter_bootstrap.js": "5b10516b1c5f015af5ecceaca5404dd6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",

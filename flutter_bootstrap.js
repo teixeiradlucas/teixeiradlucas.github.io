018347e82c2dd7f6db54ee66de4dd00290d730c1
Lucas Francisco Teixeira <63049317+teixeiradlucas@users.noreply.github.com>
2026-01-21 07:48:41 -0300

    nova versão

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3169578275"
+    serviceWorkerVersion: "3627006026"
   }
 });
